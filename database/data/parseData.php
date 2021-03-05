87f1dd6a5f2fc738722e5d3fbfece978c6d4f356
pcreem <creempepper@gmail.com>
2021-03-05 18:30:22 +0800

    feat: extract the first bookStore string data
    The further step is to
    1. Test if json_decode could work
    2. Extract all bookStore

--- database/data/parseData.php
+++ database/data/parseData.php
@@ -7,38 +7,18 @@
 
 $str = file_get_contents("./book_store_data.json");
 $str = ltrim($str,"[");
+
 $str = substr_replace($str,"",strrpos($str,","));
-echo $str;
-// var_dump(json_decode($str));
-// $json[] = $str;
-// foreach ($json as $string) {
-//     echo 'Decoding: ' . $string;
-//     json_decode($string);
+//strpos(string,find,start) 
+$pos = strpos($str,'},',strpos($str,"storeName"));
+//substr(string,start,length) 
+echo substr($str,0,$pos);
+//echo var_dump($str);
 
-//     switch (json_last_error()) {
-//         case JSON_ERROR_NONE:
-//             echo ' - No errors';
-//         break;
-//         case JSON_ERROR_DEPTH:
-//             echo ' - Maximum stack depth exceeded';
-//         break;
-//         case JSON_ERROR_STATE_MISMATCH:
-//             echo ' - Underflow or the modes mismatch';
-//         break;
-//         case JSON_ERROR_CTRL_CHAR:
-//             echo ' - Unexpected control character found';
-//         break;
-//         case JSON_ERROR_SYNTAX:
-//             echo ' - Syntax error, malformed JSON';
-//         break;
-//         case JSON_ERROR_UTF8:
-//             echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
-//         break;
-//         default:
-//             echo ' - Unknown error';
-//         break;
-//     }
+//while strrpos find position
+//split string into array 
 
-//     echo PHP_EOL;
-// }
+// $pattern = "/storeName/";
+// echo preg_match_all($pattern, $str);
+
 ?>
