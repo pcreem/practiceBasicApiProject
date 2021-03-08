d656826094cec85d4d6335a1d33eacd3ae8c804f
pcreem <creempepper@gmail.com>
2021-03-08 16:10:45 +0800

    fix: Make a correct 2 book store data output
    Create a simple data(fake.json) to test book store data output.
    Fix output formate and find the repeat pattern.
    
    resolved 0002

--- database/data/parseData.php
+++ database/data/parseData.php
@@ -7,26 +7,42 @@
 
 
 //Read data
-$str = file_get_contents("./book_store_data.json");
+$str = file_get_contents("./fake.json");
 
 //Delete "[" and "]"
 $str = ltrim($str,"[");
-$str = substr_replace($str,"",strrpos($str,","));
-//Get the first book store data end index; strpos(string,find,start) 
-$endpos = strpos($str,'},',strpos($str,"storeName"));
-//Extract the first book store data; substr(string,start,length) 
-$startpos = 0;
-$firstStore = substr($str,$startpos,$endpos)."}";
-$restStore = substr($str,$endpos+2);
+$str = substr_replace($str,"",strrpos($str,"]"));
+
+
+
+    //Get the first book store data end index; strpos(string,find,start) 
+    $endpos = strpos($str,'},',strpos($str,"storeName"));
+
+    //Extract the first book store data; substr(string,start,length) 
+    $firstStore = substr($str,0,$endpos+2);
+    echo $firstStore;
+    echo "\n";
+    $str = substr($str,$endpos+2,strlen($str));
+
+    $endpos = strpos($str,'},',strpos($str,"storeName"));
+    $firstStore = substr($str,0,$endpos+2);
+    echo $firstStore;
+    echo "\n";
+    $str = substr($str,$endpos+2,strlen($str));
+
+    
+    // $eliminateSpace = str_replace(" ","",$str);
+    
+
 
 //Extract the second book store data;
-$endpos = strpos($restStore,'},',strpos($restStore,"storeName"));
-$secondStore = substr($restStore,0,$endpos)."}";
-echo $secondStore;
+// $endpos = strpos($restStore,'},',strpos($restStore,"storeName"));
+// $secondStore = substr($restStore,0,$endpos)."}";
+// echo $secondStore;
 
-//Extract the books content
-$firstObj = json_decode($firstStore); //json_decode return an obj
-$firstArr = $firstObj->books;
+// //Extract the books content
+// $firstObj = json_decode($firstStore); //json_decode return an obj
+// $firstArr = $firstObj->books;
 
 
 // 1. Extract all bookStore
