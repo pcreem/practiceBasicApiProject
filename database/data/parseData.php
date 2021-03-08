f277cd783d2b417e73a615f2071a5d1dcde9093c
pcreem <creempepper@gmail.com>
2021-03-08 17:01:25 +0800

    fix: Fix json formate issue and use loop to process book store data
    Try to use the code to process user_data.json, but the file's formate is different from book_store_data.json.
    Delete fake.json file and some notes in code, which is useless after build the fuction.
    
    Problem: use_data.json format is differnct from book_store_data.json, need to build another code.
    
    resolved: 0002
    issue: 0003

--- database/data/parseData.php
+++ database/data/parseData.php
@@ -7,52 +7,26 @@
 
 
 //Read data
-$str = file_get_contents("./fake.json");
+$str = file_get_contents("./book_store_data.json");
 
 //Delete "[" and "]"
 $str = ltrim($str,"[");
 $str = substr_replace($str,"",strrpos($str,"]"));
+$endpos = 5;
 
-
-
-    //Get the first book store data end index; strpos(string,find,start) 
-    $endpos = strpos($str,'},',strpos($str,"storeName"));
-
-    //Extract the first book store data; substr(string,start,length) 
-    $firstStore = substr($str,0,$endpos+2);
-    echo $firstStore;
-    echo "\n";
-    $str = substr($str,$endpos+2,strlen($str));
-
-    $endpos = strpos($str,'},',strpos($str,"storeName"));
-    $firstStore = substr($str,0,$endpos+2);
-    echo $firstStore;
-    echo "\n";
-    $str = substr($str,$endpos+2,strlen($str));
-
+//Process data into json formate
+while($endpos > 4){
+    $endpos = strpos($str,',',strpos($str,"storeName"));
+    $firstStore = substr($str,0,$endpos);
     
-    // $eliminateSpace = str_replace(" ","",$str);
-    
+    if($endpos > 0){
+        $firstObj = json_decode($firstStore); 
+        $firstArr = $firstObj->books;
+        // print_r($firstArr);
+        echo $firstStore;
+        echo "\n---------------\n";
+    }
 
-
-//Extract the second book store data;
-// $endpos = strpos($restStore,'},',strpos($restStore,"storeName"));
-// $secondStore = substr($restStore,0,$endpos)."}";
-// echo $secondStore;
-
-// //Extract the books content
-// $firstObj = json_decode($firstStore); //json_decode return an obj
-// $firstArr = $firstObj->books;
-
-
-// 1. Extract all bookStore
-
-//echo var_dump($str);
-
-//while strrpos find position
-//split string into array 
-
-// $pattern = "/storeName/";
-// echo preg_match_all($pattern, $str);
-
+    $str = substr($str,$endpos+2,strlen($str));
+}
 ?>
