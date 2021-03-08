e8a196c1530f5abadbf4b40d57be0ccf1e21332f
pcreem <creempepper@gmail.com>
2021-03-08 10:23:15 +0800

    feat: Extract the second book store string data
    Code to improve: find the code repeat pattern, then make it in loop.
    Bug to fix: The file string process has a problem of eliminate the split flag "},"
    
    issue: 0002

--- database/data/parseData.php
+++ database/data/parseData.php
@@ -13,16 +13,21 @@ $str = file_get_contents("./book_store_data.json");
 $str = ltrim($str,"[");
 $str = substr_replace($str,"",strrpos($str,","));
 //Get the first book store data end index; strpos(string,find,start) 
-$pos = strpos($str,'},',strpos($str,"storeName"));
+$endpos = strpos($str,'},',strpos($str,"storeName"));
 //Extract the first book store data; substr(string,start,length) 
-$firstStore = substr($str,0,$pos)."}";
+$startpos = 0;
+$firstStore = substr($str,$startpos,$endpos)."}";
+$restStore = substr($str,$endpos+2);
+
+//Extract the second book store data;
+$endpos = strpos($restStore,'},',strpos($restStore,"storeName"));
+$secondStore = substr($restStore,0,$endpos)."}";
+echo $secondStore;
+
+//Extract the books content
 $firstObj = json_decode($firstStore); //json_decode return an obj
 $firstArr = $firstObj->books;
-foreach($firstArr as $value)
-{
-    echo $value->bookName;
-    echo "\n";
-}
+
 
 // 1. Extract all bookStore
 
