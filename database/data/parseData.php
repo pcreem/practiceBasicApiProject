175d6b7379e66873e6dbbcffdf7beca217abb20a
pcreem <creempepper@gmail.com>
2021-03-08 09:32:11 +0800

    feat: Extract data from first book store json data
    The furthur step is to extract all book store data

--- database/data/parseData.php
+++ database/data/parseData.php
@@ -5,14 +5,27 @@
 // write API document
 // build API
 
-$str = file_get_contents("./book_store_data.json");
-$str = ltrim($str,"[");
 
+//Read data
+$str = file_get_contents("./book_store_data.json");
+
+//Delete "[" and "]"
+$str = ltrim($str,"[");
 $str = substr_replace($str,"",strrpos($str,","));
-//strpos(string,find,start) 
+//Get the first book store data end index; strpos(string,find,start) 
 $pos = strpos($str,'},',strpos($str,"storeName"));
-//substr(string,start,length) 
-echo substr($str,0,$pos);
+//Extract the first book store data; substr(string,start,length) 
+$firstStore = substr($str,0,$pos)."}";
+$firstObj = json_decode($firstStore); //json_decode return an obj
+$firstArr = $firstObj->books;
+foreach($firstArr as $value)
+{
+    echo $value->bookName;
+    echo "\n";
+}
+
+// 1. Extract all bookStore
+
 //echo var_dump($str);
 
 //while strrpos find position
